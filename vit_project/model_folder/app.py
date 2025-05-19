9cd4a7599f5166f459d604c5bf963671aff5a2cf
AnushaPaladugu <136462313+AnushaPaladugu@users.noreply.github.com>
2025-05-19 22:24:34 +0530

    Update app.py

--- vit_project/model_folder/app.py
+++ vit_project/model_folder/app.py
@@ -741,4 +741,4 @@ if __name__ == '__main__':
             print(f"WARNING: 2d-3d.py not found at {script_path} or {alt_path}")
     
     print("Starting Flask server on http://127.0.0.1:5000/")
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
